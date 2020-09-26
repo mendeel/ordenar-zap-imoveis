da9f61143f30871a808a1707e7ff85b35c60e75a
Daniel Mendel <1301796+mendeel@users.noreply.github.com>
2020-09-26 19:07:05 -0300

    Update ordenar.js
    Adicionado bloco try/catch

--- ordenar.js
+++ ordenar.js
@@ -1,26 +1,28 @@
 // Gerar array com todos os cards
 Array.from(document.querySelectorAll(".card-container")).forEach(card => {
-    // Preço por mês
-    let monthPrice = parseInt(card.querySelector(".js-price").textContent.trim().replace(/\./gi, "").match(/([0-9]+)/gi)[0]);
-    
-    // Preço do condomínio
-    let condoPrice = parseInt(card.querySelector(".prices--regular .condominium").textContent.replace(/\./gi, "").trim().match(/([0-9]+)/gi)[0]);
-    
-    // Preço total
-    let totalPrice = monthPrice + condoPrice;
-    
-    // Criar elemento para inserir o preço total no card
-    let totalPriceEl = document.createElement("li");
-    
-    // Colocar preço total como atributo do card para usar na hora de ordenar os resultados
-    card.setAttribute("totalPrice", totalPrice);
-    
-    // Ajustar as classes do elemento novo
-    totalPriceEl.classList = "card-price__item totalPrice text-regular";
-    
-    // Inserir conteúdo do elemento novo
-    totalPriceEl.innerHTML = `<span class="card-price__value"><b>TOTAL R$ ${totalPrice}</b></span>`;
-    card.querySelector("ul.prices.prices--regular").appendChild(totalPriceEl);
+    try {
+        // Preço por mês
+        let monthPrice = parseInt(card.querySelector(".js-price").textContent.trim().replace(/\./gi, "").match(/([0-9]+)/gi)[0]);
+
+        // Preço do condomínio
+        let condoPrice = parseInt(card.querySelector(".prices--regular .condominium").textContent.replace(/\./gi, "").trim().match(/([0-9]+)/gi)[0]);
+
+        // Preço total
+        let totalPrice = monthPrice + condoPrice;
+
+        // Criar elemento para inserir o preço total no card
+        let totalPriceEl = document.createElement("li");
+
+        // Colocar preço total como atributo do card para usar na hora de ordenar os resultados
+        card.setAttribute("totalPrice", totalPrice);
+
+        // Ajustar as classes do elemento novo
+        totalPriceEl.classList = "card-price__item totalPrice text-regular";
+
+        // Inserir conteúdo do elemento novo
+        totalPriceEl.innerHTML = `<span class="card-price__value"><b>TOTAL R$ ${totalPrice}</b></span>`;
+        card.querySelector("ul.prices.prices--regular").appendChild(totalPriceEl);
+    } catch (ex) {}
 })
 
 // Ordenação
